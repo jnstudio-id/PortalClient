11132f0c76e35f4d75d6bb60e52c1c5ad626594b
jnstudio-id <jn.stud10000@gmail.com>
2026-07-28 19:27:59 +0700

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -8,6 +8,6 @@ const CONFIG_APP = {
     namaVendor: "JN STUDIO",
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
-    googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo",
+    googleApiKey: "AIzaSyCpVVtI66F1_xWReSTlQblQP5MxWappROo",
     whatsappAdmin: "62821000000000" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
 };
